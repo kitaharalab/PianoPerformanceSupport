63ad7428be1592ce88d7e18067198c85e29ee393
fujiij <32182215+fujiij@users.noreply.github.com>
2024-12-06 12:45:30 +0900

    20241206

---
+++ src/TestSimplePianoRoll.java
@@ -0,0 +1,9 @@
+import processing.core.*;
+import jp.crestmuse.cmx.processing.gui.SimplePianoRoll;
+
+public class TestSimplePianoRoll extends SimplePianoRoll {
+
+  public static void main(String[] args) {
+    PApplet.main(new String[] { TestSimplePianoRoll.class.getName() });
+  }
+}
